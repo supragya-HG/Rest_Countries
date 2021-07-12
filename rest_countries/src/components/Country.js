66d26a180888aa63de773c8a4d39885fa29994ee
supragya-Hg <supragya.shrestha@housing.com>
2021-07-12 17:13:10 +0530

    countryDataDiv creation tried

--- rest_countries/src/components/Country.js
+++ rest_countries/src/components/Country.js
@@ -10,10 +10,12 @@ function Country(props){
             <div className='flag'>
                 <img src = {props.flagUrl} alt = 'flag' className='flagImage'/>
             </div>
-            <div className='countryName'>{props.name}</div>
-            <div className='countryInfo'>Population: <span className='countryInfoData'>{props.population}</span></div>
-            <div className='countryInfo'>Region: <span className='countryInfoData'>{props.region}</span></div>
-            <div className='countryInfo'>Capital: <span className='countryInfoData'>{props.capital}</span></div>
+            {/* <div className='countryDataDiv'> */}
+                <div className='countryName'>{props.name}</div>
+                <div className='countryInfo'>Population: <span className='countryInfoData'>{props.population}</span></div>
+                <div className='countryInfo'>Region: <span className='countryInfoData'>{props.region}</span></div>
+                <div className='countryInfo'>Capital: <span className='countryInfoData'>{props.capital}</span></div>
+            {/* </div> */}
         </div>
     );
 }
